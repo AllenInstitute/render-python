863062391e69c416f0f76ca7de110d0ed8fd562e
Forrest Collman <forrestc@alleninstitute.org>
2017-01-09 10:57:17 -0800

    reformated for clarity

--- create_mipmaps.py
+++ create_mipmaps.py
@@ -1,7 +1,32 @@
 from PIL import Image
 import argparse
 import os
+from tilespec import TileSpec
 
+#def add_mipmaps_to_tilespec(tilespec):
+    
+
+def create_mipmaps(inputImage,outputDirectory='.',mipmaplevels=[1,2,3],outputformat='jpg'):
+    if not os.path.isdir(outputDirectory):
+        os.makedirs(outputDirectory)
+
+    im = Image.open(inputImage)
+    #print 'origmode',im.mode
+    origsize = im.size
+    table=[ i/256 for i in range(65536) ]
+    im = im.convert('I')
+    im = im.point(table,'L')
+    #print 'new mode',im.mode
+    inputFileName = os.path.split(inputImage)[1]
+                   
+    for level in mipmaplevels:
+        newsize = tuple(map(lambda x: x/(2**level), origsize))
+        dwnImage = im.resize(newsize)
+        outpath = os.path.join(outputDirectory,inputFileName[0:-4]+'_mip%02d.'%level+outputformat)
+        dwnImage.save(outpath)
+        print outpath,level,newsize
+
+        
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Create downsampled images of the input image at different midmap levels")
 
@@ -13,29 +38,31 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     
-    inputFileName = os.path.split(args.inputImage)[1]
-    im = Image.open(args.inputImage)
-    print 'origmode',im.mode
-    origsize = im.size
-    table=[ i/256 for i in range(65536) ]
-    im = im.convert('I')
-    im = im.point(table,'L')
-    print 'new mode',im.mode
     print 'outdir',args.outputDirectory
     if args.outputDirectory is None:
         args.outputDirectory = os.path.split(args.inputImage)[0]
         if len(args.outputDirectory)==0:
             args.outputDirectory='.'
 
-    if not os.path.isdir(args.outputDirectory):
-        os.makedirs(args.outputDirectory)
-
+    create_mipmaps(args.inputImage,args.outputDirectory,args.mipmaplevels,args.outputformat)
     
-    for level in args.mipmaplevels:
-        newsize = tuple(map(lambda x: x/(2**level), origsize))
-        dwnImage = im.resize(newsize)
-        outpath = os.path.join(args.outputDirectory,inputFileName[0:-4]+'_mip%02d.'%level+args.outputformat)
-        dwnImage.save(outpath)
-        print outpath,level,newsize
+#     if not os.path.isdir(args.outputDirectory):
+#         os.makedirs(args.outputDirectory)
+
+#     im = Image.open(args.inputImage)
+#     print 'origmode',im.mode
+#     origsize = im.size
+#     table=[ i/256 for i in range(65536) ]
+#     im = im.convert('I')
+#     im = im.point(table,'L')
+#     print 'new mode',im.mode
+#     inputFileName = os.path.split(args.inputImage)[1]
+                   
+#     for level in args.mipmaplevels:
+#         newsize = tuple(map(lambda x: x/(2**level), origsize))
+#         dwnImage = im.resize(newsize)
+#         outpath = os.path.join(args.outputDirectory,inputFileName[0:-4]+'_mip%02d.'%level+args.outputformat)
+#         dwnImage.save(outpath)
+#         print outpath,level,newsize
         
     
