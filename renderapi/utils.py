236b2d9f12d6a31fc53cd6bfca18f5d40e9e974d
RussTorres <russtorres@gmail.com>
2017-02-15 12:39:49 -0800

    utils: doctring and logging

--- renderapi/utils.py
+++ renderapi/utils.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python
 '''
-utilities to make render/java/web interfacing easier
+utilities to make render/java/web/life interfacing easier
 '''
 import logging
 
+logger = logging.getLogger(__name__)
+
 
 def jbool(val):
-    '''return string representing json string values of py booleans'''
+    '''return string representing java string values of py booleans'''
+    if not isinstance(val, bool):
+        logger.warning('Evaluating javastring of non-boolean {} {}'.format(
+            type(val), val))
     return 'true' if val else 'false'
 
 
 def stripLogger(logger):
-    '''remove all handlers from a logger -- useful for redefining'''
+    '''
+    remove all handlers from a logger -- useful for redefining
+    input:
+        logger: logging logger as from logging.getLogger
+    '''
     if logger.handlers:
         for handler in logger.handlers:
             logger.removeHandler(handler)
