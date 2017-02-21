49c7328c8c33b292e15b4a6a07c10161d778acc9
Forrest Collman <forrestc@alleninstitute.org>
2017-02-21 10:01:23 -0800

    fixing requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from distutils.core import setup
+from setuptools import setup
 setup(name='render-python',
 	version='1.0',
 	description=' a python API setup to interact via python with render databases see https://github.com/saalfeldlab/render',
 	author='Forrest Collman,Eric Perlman,Sharmi Seshamani',
 	author_email='forrest.collman@gmail.com',
 	url='https://github.com/fcollman/render-python',
+	install_requires=['requests','pillow','numpy','pathos'],
 	packages = ['renderapi'])
