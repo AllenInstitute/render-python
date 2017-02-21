ff88554f7311fc5d16134433ba5b4494e4a99f64
RussTorres <russtorres@gmail.com>
2017-02-20 16:46:46 -0800

    setup: PEP8

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from distutils.core import setup
 setup(name='render-python',
-	version='1.0',
-	description=' a python API setup to interact via python with render databases see https://github.com/saalfeldlab/render',
-	author='Forrest Collman,Eric Perlman,Sharmi Seshamani',
-	author_email='forrest.collman@gmail.com',
-	url='https://github.com/fcollman/render-python',
-	packages = ['renderapi'])
+      version='1.0',
+      description=' a python API setup to interact via python with render '
+                  'databases see https://github.com/saalfeldlab/render',
+      author='Forrest Collman,Eric Perlman,Sharmi Seshamani',
+      author_email='forrest.collman@gmail.com',
+      url='https://github.com/fcollman/render-python',
+      packages=['renderapi'])
