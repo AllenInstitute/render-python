0797425aa14f38b12b7df2aaca049ed68465f2b4
RussTorres <russtorres@gmail.com>
2017-02-23 09:29:03 -0800

    setup: read from requirements.txt

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup
+
+with open('requirements.txt', 'r') as f:
+    required = f.read().splitlines()
+
 setup(name='render-python',
       version='1.0',
       description=' a python API setup to interact via python with render '
@@ -6,4 +10,5 @@ setup(name='render-python',
       author='Forrest Collman,Eric Perlman,Sharmi Seshamani',
       author_email='forrest.collman@gmail.com',
       url='https://github.com/fcollman/render-python',
-      packages=['renderapi'])
+      packages=['renderapi'],
+      install_requires=required)
