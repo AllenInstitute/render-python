88dcc9f6f4103fcab87b062f328c72c316f6c5b8
Forrest Collman <forrestc@alleninstitute.org>
2017-02-24 10:27:31 -0800

    adding in client side coordinate mapping options

--- renderapi/coordinate.py
+++ renderapi/coordinate.py
@@ -9,6 +9,7 @@ import requests
 import json
 import numpy as np
 import logging
+import tempfile
 logger = logging.getLogger(__name__)
 
 
@@ -55,7 +56,7 @@ def local_to_world_coordinates(stack, tileId, x, y, render=None,
         logger.error(r.text)
 
 
-def world_to_local_coordinates_batch(stack, z, data, render=None, host=None,
+def world_to_local_coordinates_batch(stack, data,z, render=None, host=None,
                                      port=None, owner=None, project=None, execute_local=True,
                                      session=requests.session(), **kwargs):
     ''''''
@@ -69,7 +70,7 @@ def world_to_local_coordinates_batch(stack, z, data, render=None, host=None,
 
     request_url = format_preamble(
         host, port, owner, project, stack) + \
-        "/z/%d/world-to-local-coordinates" % (z)
+        "/z/%s/world-to-local-coordinates" % (str(z))
     r = session.put(request_url, data=data,
                     headers={"content-type": "application/json"})
     return r.json()
@@ -89,10 +90,13 @@ def local_to_world_coordinates_batch(stack, data, z, render=None, host=None,
 
     request_url = format_preamble(
         host, port, owner, project, stack) + \
-        "/z/%d/local-to-world-coordinates" % (z)
+        "/z/%s/local-to-world-coordinates" % (str(z))
     r = session.put(request_url, data=data,
                     headers={"content-type": "application/json"})
-    return r.json()
+    try:
+        return r.json()
+    except:
+        logger.error(r.text)
 
 def package_point_match_data_into_json(dataarray,tileId,local_or_world='local'):
     dlist = []
@@ -104,7 +108,9 @@ def package_point_match_data_into_json(dataarray,tileId,local_or_world='local'):
     return json.dumps(dlist)
 
 def unpackage_point_match_data_from_json(json_answer,local_or_world='local'):
-    answer = np.zeros((len(json_answer,2)))
+    answer = np.zeros((len(json_answer),2))
+    if type(json_answer[0]) == list:
+        json_answer = json_answer[0]
     for i, coord in enumerate(json_answer):
         c = coord[local_or_world]
         answer[i, 0] = c[0]
@@ -114,7 +120,7 @@ def unpackage_point_match_data_from_json(json_answer,local_or_world='local'):
 def world_to_local_coordinates_array(stack, dataarray, tileId, z,
                                      render=None, host=None, port=None,
                                      owner=None, project=None, client_script = None,
-                                     doClientSide = False,
+                                     doClientSide = False,number_of_threads=20,
                                      session=requests.session(), **kwargs):
     ''''''
 
@@ -125,18 +131,25 @@ def world_to_local_coordinates_array(stack, dataarray, tileId, z,
             stack, dataarray, tileId, z, **render.make_kwargs(
                 host=host, port=port, owner=owner, project=project,
                 client_script = client_script,
-                **{'session': session,'doClientSide':doClientSide}))
+                **{'session': session,'doClientSide':doClientSide,'number_of_threads':20}))
 
     jsondata =   package_point_match_data_into_json(dataarray,tileId,'world')
     if doClientSide:
-        json_answer = world_to_local_coordinates_clientside(stack,jsondata,tileId,z,host,port,owner,client_script,number_of_threads=number_of_threads)
+        json_answer = world_to_local_coordinates_clientside(stack,jsondata,tileId,z,
+                                                            host=host,port=port,owner=owner,
+                                                            project=project,
+                                                            client_script=client_script,
+                                                            number_of_threads=number_of_threads)
     else:
-        json_answer = world_to_local_coordinates_batch(stack,z,jsondata,host,port,owner,project,session)
-
-    try:
-        return unpackage_point_match_data_from_json(json_answer,'local')
-    except:
-        logger.error(json_answer)
+        json_answer = world_to_local_coordinates_batch(stack,jsondata,z,
+                                                       host=host,port=port,
+                                                       owner=owner,project=project,
+                                                       session=session)
+    #try:
+    return unpackage_point_match_data_from_json(json_answer,'local')
+    #except:
+    #    logger.error('could not unpackage world_to_local answer')
+    #    logger.error(json_answer)
 
 def local_to_world_coordinates_array(stack, dataarray, tileId, z,
                                      render=None, host=None, port=None,
@@ -155,47 +168,54 @@ def local_to_world_coordinates_array(stack, dataarray, tileId, z,
 
     jsondata = package_point_match_data_into_json(dataarray,tileId,'local')
     if doClientSide:
-        json_answer = local_to_world_coordinates_clientside(stack,jsondata,tileId,z,host,owner,project,client_script,number_of_threads=number_of_threads)
+        json_answer = local_to_world_coordinates_clientside(stack,jsondata,tileId,z,
+                                                            host=host,port=port,owner=owner,project=project,
+                                                            client_script=client_script,
+                                                            number_of_threads=number_of_threads)
     else:
-        json_answer = local_to_world_coordinates_batch(stack,z,jsondata,host,port,owner,project,session)
-    try:
-        return unpackage_point_match_data_from_json(json_answer,'world')
-    except:
-        logger.error(json_answer)
+        json_answer = local_to_world_coordinates_batch(stack,jsondata,z,
+                                                       host=host,port=port,owner=owner,
+                                                       project=project,session=session)
+    #try:
+    return unpackage_point_match_data_from_json(json_answer,'world')
+    #except:
+    #    logger.error('could not unpackage local_to_world answer')
+    #    logger.error(json_answer)
 
 def map_coordinates_clientside(stack,jsondata,tileId,z,host,port,owner,project,client_script,isLocalToWorld=False,number_of_threads=20):
     #write point match json to temp file on disk
     json_infile,json_inpath = tempfile.mkstemp(prefix='render_coordinates_in_',suffix='.json')
-    json.dump(jsondata,json_infile)
-    close(json_infile)
+    fp = open(json_inpath,'w')
+    fp.write(jsondata)
+    fp.close()
+
+    #json.dump(jsondata,open(json_inpath,'w'))
 
     #get a temporary location for the output
     json_outpath = tempfile.mktemp(prefix='render_coordinates_out_',suffix='.json')
 
-    if isLocalToWorld:
-        localToWorld = 'true'
-    else:
-        localToWorld = 'false'
+
 
     #define arguments
-    args = ['--baseDataUrl','https://%s:%d/render-ws/v1'%(host,port),
+    args = ['--baseDataUrl','http://%s:%d/render-ws/v1'%(host,port),
             '--owner',owner,
             '--project',project,
             '--stack',stack,
             '--z',str(z),
             '--fromJson',json_inpath,
             '--toJson',json_outpath,
-            '--localToWorld',localToWorld,
             '--numberOfThreads',str(number_of_threads)]
+    if isLocalToWorld:
+        args+=['--localToWorld']
 
     #call the java client
-    call_run_ws_client('org.janelia.render.client', add_args=args, client_script=client_script)
+    call_run_ws_client('org.janelia.render.client.CoordinateClient', add_args=args, client_script=client_script)
 
     #return the json results
-    try:
-        return json.load(open(json_outpath,'r'))
-    except:
-        logger.error('failed to load json after map_coordinates_clientside:\n%s'%json.dumps(jsondata))
+    #try:
+    return json.load(open(json_outpath,'r'))
+    #except:
+    #    logger.error('failed to load json after map_coordinates_clientside:\n%s'%json.dumps(jsondata))
 
 def world_to_local_coordinates_clientside(stack,jsondata,tileId,z,render=None,
                                           host=None,port=None,owner=None,
@@ -211,7 +231,10 @@ def world_to_local_coordinates_clientside(stack,jsondata,tileId,z,render=None,
                 client_script=client_script,
                 **{'session': session,'number_of_threads':20}))
 
-    return map_coordinates_clientside(stack,jsondata,tileId,z,host,port,owner,project,client_script,isLocalToWorld=False,number_of_threads=number_of_threads)
+    return map_coordinates_clientside(stack,jsondata,tileId,z,
+                                      host=host,port=port,owner=owner,
+                                      project=project,client_script=client_script,
+                                      isLocalToWorld=False,number_of_threads=number_of_threads)
 
 def local_to_world_coordinates_clientside(stack,jsondata,tileId,z,render=None,
                                           host=None,port=None,owner=None,
@@ -227,4 +250,7 @@ def local_to_world_coordinates_clientside(stack,jsondata,tileId,z,render=None,
                 client_script=client_script,
                 **{'session': session,'number_of_threads':20}))
 
-    return map_coordinates_clientside(stack,jsondata,tileId,z,host,port,owner,project,client_script,isLocalToWorld=True,number_of_threads=number_of_threads)
+    return map_coordinates_clientside(stack,jsondata,tileId,z,
+                                      host=host,port=port,owner=owner,
+                                      project=project,client_script=client_script,
+                                      isLocalToWorld=True,number_of_threads=number_of_threads)
