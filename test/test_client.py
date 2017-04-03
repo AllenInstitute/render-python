023e9891d2a37c651842a47716bb3de7749b1543
RussTorres <russtorres@gmail.com>
2017-04-03 12:50:15 -0700

    tests: PEP8

--- test/test_client.py
+++ test/test_client.py
@@ -1,11 +1,12 @@
 import renderapi
 
+
 def test_render_client():
-    args={
-        'host':'renderhost',
-        'port':8080,
-        'owner':'renderowner',
-        'project':'renderproject',
-        'client_scripts':'/path/to/client_scripts'
-    }
+    args = {
+        'host': 'renderhost',
+        'port': 8080,
+        'owner': 'renderowner',
+        'project': 'renderproject',
+        'client_scripts': '/path/to/client_scripts'
+        }
     r = renderapi.render.connect(**args)
