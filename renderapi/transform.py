2ac3632f5414306d484e62e0e7ed0cbc73976620
RussTorres <russtorres@gmail.com>
2017-05-18 15:21:42 -0700

    transform: estimation for Rigid, Translation, and Similarity 2D with tests

--- renderapi/transform.py
+++ renderapi/transform.py
@@ -80,9 +80,12 @@ def load_transform_json(d, default_type='leaf'):
 
 def load_leaf_json(d):
     handle_load_leaf = {
-        AffineModel.className: lambda x: AffineModel(json=d),
+        AffineModel.className: lambda x: AffineModel(json=x),
         Polynomial2DTransform.className:
-            lambda x: Polynomial2DTransform(json=d)}
+            lambda x: Polynomial2DTransform(json=x),
+        TranslationModel.className: lambda x: TranslationModel(json=x),
+        RigidModel.className: lambda x: RigidModel(json=x),
+        SimilarityModel.className: lambda x: SimilarityModel(json=x)}
 
     tform_type = d.get('type', 'leaf')
     if tform_type != 'leaf':
@@ -242,13 +245,8 @@ class AffineModel(Transform):
         self.M[0, 2] = self.B0
         self.M[1, 2] = self.B1
 
-    def invert(self):
-        inv_M = np.linalg.inv(self.M)
-        Ai = AffineModel(inv_M[0, 0], inv_M[0, 1], inv_M[1, 0],
-                         inv_M[1, 1], inv_M[0, 2], inv_M[1, 2])
-        return Ai
-
-    def fit(self, A, B):
+    @staticmethod
+    def fit(A, B):
         if not all([A.shape[0] == B.shape[0], A.shape[1] == B.shape[1] == 2]):
             raise EstimationError(
                 'shape mismatch! A shape: {}, B shape {}'.format(
@@ -267,10 +265,8 @@ class AffineModel(Transform):
         (Tvec, residuals, rank, s) = np.linalg.lstsq(M, Y)
         return Tvec
 
-    def estimate(self, A, B):
-        Tvec = self.fit(A, B)
-        # t = numpy.array([Tvec[4,0],Tvec[5,0]])
-        # R = numpy.array([[Tvec[0,0],Tvec[1,0]],[Tvec[2,0],Tvec[3,0]]])
+    def estimate(self, A, B, return_params=True, **kwargs):
+        Tvec = self.fit(A, B, **kwargs)
         self.M00 = Tvec[0, 0]
         self.M10 = Tvec[2, 0]
         self.M01 = Tvec[1, 0]
@@ -278,27 +274,8 @@ class AffineModel(Transform):
         self.B0 = Tvec[4, 0]
         self.B1 = Tvec[5, 0]
         self.load_M()
-        return self.M
-
-    def convert_to_point_vector(self, points):
-        Np = points.shape[0]
-        onevec = np.ones((Np, 1), np.double)
-
-        if points.shape[1] != 2:
-            raise ConversionError('Points must be of shape (:, 2) '
-                                  '-- got {}'.format(points.shape))
-        Nd = 2
-        points = np.concatenate((points, onevec), axis=1)
-        return points, Nd
-
-    def convert_points_vector_to_array(self, points, Nd):
-        points = points[:, 0:Nd] / np.tile(points[:, 2], (Nd, 1)).T
-        return points
-
-    def tform(self, points):
-        points, Nd = self.convert_to_point_vector(points)
-        pt = np.dot(self.M, points.T).T
-        return self.convert_points_vector_to_array(pt, Nd)
+        if return_params:
+            return self.M
 
     def concatenate(self, model):
         '''
@@ -323,6 +300,34 @@ class AffineModel(Transform):
 
         newmodel = AffineModel(a00, a01, a10, a11, a02, a12)
         return newmodel
+
+    def invert(self):
+        inv_M = np.linalg.inv(self.M)
+        Ai = AffineModel(inv_M[0, 0], inv_M[0, 1], inv_M[1, 0],
+                         inv_M[1, 1], inv_M[0, 2], inv_M[1, 2])
+        return Ai
+
+    @staticmethod
+    def convert_to_point_vector(points):
+        Np = points.shape[0]
+        onevec = np.ones((Np, 1), np.double)
+
+        if points.shape[1] != 2:
+            raise ConversionError('Points must be of shape (:, 2) '
+                                  '-- got {}'.format(points.shape))
+        Nd = 2
+        points = np.concatenate((points, onevec), axis=1)
+        return points, Nd
+
+    @staticmethod
+    def convert_points_vector_to_array(points, Nd):
+        points = points[:, 0:Nd] / np.tile(points[:, 2], (Nd, 1)).T
+        return points
+
+    def tform(self, points):
+        points, Nd = self.convert_to_point_vector(points)
+        pt = np.dot(self.M, points.T).T
+        return self.convert_points_vector_to_array(pt, Nd)
 
     def inverse_tform(self, points):
         points, Nd = self.convert_to_point_vector(points)
@@ -356,6 +361,106 @@ class AffineModel(Transform):
             self.M[1, 1], self.M[0, 2], self.M[1, 2])
 
 
+class TranslationModel(AffineModel):
+    className = 'mpicbg.trakem2.transform.TranslationModel2D'
+
+    def __init__(self, *args, **kwargs):
+        super(TranslationModel, self).__init__(*args, **kwargs)
+        # raise NotImplementedError(
+        #     'TranslationModel not implemented. please use Affine')
+
+    def _process_dataString(self, dataString):
+        '''expected dataString is tx, ty'''
+        raise NotImplementedError
+
+    @staticmethod
+    def fit(src, dst):
+        t = dst.mean(axis=0) - src.mean(axis=0)
+        T = np.eye(3)
+        T[:2, 2] = t
+        return T
+
+    def estimate(self, src, dst, return_params=True):
+        self.M = self.fit(src, dst)
+        if return_params:
+            return self.M
+
+
+class RigidModel(AffineModel):
+    className = 'mpicbg.trakem2.transform.RigidModel2D'
+
+    def __init__(self, *args, **kwargs):
+        super(RigidModel, self).__init__(*args, **kwargs)
+        # raise NotImplementedError(
+        #     'RigidModel not implemented. please use Affine')
+
+    def _process_dataString(self, dataString):
+        '''expected datastring is theta, tx, ty'''
+        raise NotImplementedError
+
+    @staticmethod
+    def fit(src, dst, rigid=True):
+        '''
+        Umeyama estimation of similarity transformation
+        '''
+        # TODO shape assertion
+        num, dim = src.shape
+        src_cld = src - src.mean(axis=0)
+        dst_cld = dst - dst.mean(axis=0)
+        A = np.dot(dst_cld.T, src_cld) / num
+        d = np.ones((dim, ), dtype=np.double)
+        if np.linalg.det(A) < 0:
+            d[dim - 1] = -1
+        T = np.eye(dim + 1, dtype=np.double)
+
+        rank = np.linalg.matrix_rank(A)
+        if rank == 0:
+            raise EstimationError('zero rank matrix A unacceptable -- '
+                                  'likely poorly conditioned')
+
+        U, S, V = svd(A)
+
+        if rank == dim - 1:
+            if np.linalg.det(U) * np.linalg.det(V) > 0:
+                T[:dim, :dim] = np.dot(U, V)
+            else:
+                s = d[dim - 1]
+                d[dim - 1] = -1
+                T[:dim, :dim] = np.dot(U, np.dot(np.diag(d), V))
+                d[dim - 1] = s
+        else:
+            T[:dim, :dim] = np.dot(U, np.dot(np.diag(d), V.T))
+
+        fit_scale = (1.0 if rigid else
+                     1.0 / src_cld.var(axis=0).sum() * np.dot(S, d))
+
+        T[:dim, dim] = dst.mean(axis=0) - fit_scale * np.dot(
+            T[:dim, :dim], src.mean(axis=0).T)
+        T[:dim, :dim] *= fit_scale
+        return T
+
+    def estimate(self, A, B, return_params=True, **kwargs):
+        self.M = self.fit(A, B, **kwargs)
+        if return_params:
+            return self.M
+
+
+class SimilarityModel(RigidModel):
+    className = 'mpicbg.trakem2.transform.SimilarityModel2D'
+
+    def __init__(self, *args, **kwargs):
+        super(SimilarityModel, self).__init__(*args, **kwargs)
+        # raise NotImplementedError(
+        #     'SimilarityModel not implemented. please use Affine')
+
+    def _process_dataString(self, dataString):
+        '''expected datastring is s, theta, tx, ty'''
+        raise NotImplementedError
+
+    def estimate(self, src, dst, **kwargs):
+        super(SimilarityModel, self).estimate(src, dst, rigid=False, **kwargs)
+
+
 class Polynomial2DTransform(Transform):
     '''
     Polynomial2DTransform implemented as in skimage
@@ -372,7 +477,6 @@ class Polynomial2DTransform(Transform):
     2nd
     dataString = dataString representation of transform from mpicpg
 
-
     3rd
     identity = make this transform the identity
 
@@ -387,7 +491,6 @@ class Polynomial2DTransform(Transform):
 
     TODO:
         fall back to Affine Model in special cases
-        robustness in estimation
     '''
     className = 'mpicbg.trakem2.transform.PolynomialTransform2D'
 
@@ -405,7 +508,7 @@ class Polynomial2DTransform(Transform):
             elif params is not None:
                 self.params = params
             elif src is not None and dst is not None:
-                self.params = self.estimate(src, dst, order, **kwargs)
+                self.estimate(src, dst, order, return_params=False, **kwargs)
 
             if not force_polynomial and self.is_affine:
                 raise NotImplementedError('Falling back to Affine model is '
@@ -429,7 +532,6 @@ class Polynomial2DTransform(Transform):
 
     @staticmethod
     def fit(src, dst, order=2):
-        '''This is unreliable -- add tests to ensure repeatability'''
         xs = src[:, 0]
         ys = src[:, 1]
         xd = dst[:, 0]
@@ -458,19 +560,18 @@ class Polynomial2DTransform(Transform):
         A[rows:, -1] = yd
 
         # right singular vector corresponding to smallest singular value
-        # TODO implement tests for this
         _, s, V = svd(A)
         Vsm = V[np.argmin(s), :]  # never trust computers
         return (-Vsm[:-1] / Vsm[-1]).reshape((2, no_coeff // 2))
-        # return (-V[-1, :-1] / V[-1, -1]).reshape((2, no_coeff // 2))
 
     def estimate(self, src, dst, order=2,
-                 convergence_test=None, max_tries=100, **kwargs):
-        def fitgood(src, dst, params):
+                 test_coords=True, max_tries=100, return_params=True,
+                 **kwargs):
+        def fitgood(src, dst, params, atol=1e-3, rtol=0, **kwargs):
             result = Polynomial2DTransform(params=params).tform(src)
             t = np.allclose(
                 result, dst,
-                atol=1e-3, rtol=0)
+                atol=atol, rtol=rtol)
             return t
 
         estimated = False
@@ -482,14 +583,16 @@ class Polynomial2DTransform(Transform):
             except (LinAlgError, ValueError) as e:
                 logger.debug('Encountered error {}'.format(e))
                 continue
-            estimated = fitgood(src, dst, params)
+            estimated = (fitgood(src, dst, params, **kwargs) if
+                         test_coords else True)
 
         if tries == max_tries and not estimated:
             raise EstimationError('Could not fit Polynomial '
                                   'in {} attempts!'.format(tries))
         logger.debug('fit parameters in {} attempts'.format(tries))
         self.params = params
-        return self.params
+        if return_params:
+            return self.params
 
     @staticmethod
     def _dataStringfromParams(params=None):
@@ -604,6 +707,6 @@ def estimate_transformsum(transformlist, src=None, order=2):
     if all([tform.className == AffineModel.className
             for tform in tforms]):
                 am = AffineModel()
-                _ = am.estimate(A=src, B=dstpts)
+                am.estimate(A=src, B=dstpts, return_params=False)
                 return am
     return Polynomial2DTransform(src=src, dst=dstpts, order=order)
