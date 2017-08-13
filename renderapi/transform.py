149d845a8dd93bb3ec5b585f37ee4a5f79df7340
forrest collman <forrest.collman@gmail.com>
2017-08-12 17:12:04 -0700

    typo

--- renderapi/transform.py
+++ renderapi/transform.py
@@ -162,7 +162,7 @@ def load_leaf_json(d):
         TranslationModel.className: lambda x: TranslationModel(json=x),
         RigidModel.className: lambda x: RigidModel(json=x),
         SimilarityModel.className: lambda x: SimilarityModel(json=x),
-        NonLinearTransform.className lambda x: NonLinearTransform(json=x)}
+        NonLinearTransform.className: lambda x: NonLinearTransform(json=x)}
 
     tform_type = d.get('type', 'leaf')
     if tform_type != 'leaf':
