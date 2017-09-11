c5ae6a272bddb980e13bdf12cc4b590c58bf0fd3
forrest collman <forrest.collman@gmail.com>
2017-09-11 14:13:59 -0700

    fixed test of resolved tiles

--- test/test_resolvedtiles.py
+++ test/test_resolvedtiles.py
@@ -5,4 +5,13 @@ import rendersettings
 
 def test_resolved_tiles():
     
-    tile = renderapi.tilespec.TileSpec()
+    with open(rendersettings.REFERENCE_TRANSFORM_TILESPECS,'r') as fp:
+        ds = json.load(fp)
+        tilespecs = [renderapi.tilespec.TileSpec(json=d) for d in ds]
+
+    with open(rendersettings.REFERENCE_TRANSFORM_SPECS,'r') as fp:
+        ds = json.load(fp)
+        transforms = [renderapi.transform.load_transform_json(tf) for tf in ds]
+    
+    resolved_tiles = renderapi.resolvedtiles.ResolvedTiles(tilespecs =tilespecs,
+                                                           transformList=transforms)
