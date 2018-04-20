2f1e530624b9a6a1fbdb324200b4d44993aaffba
Forrest Collman <forrest.collman@gmail.com>
2018-04-20 13:34:29 -0700

    fixing bad pytest dump test (#100)

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,5 +1,6 @@
 import renderapi
 import pytest
+import numpy as np
 
 def test_jbool():
     assert(renderapi.utils.jbool(True) == 'true')
@@ -16,4 +17,4 @@ def test_renderdumps_simple():
 
 def test_renderdumps_fails():
     with pytest.raises(AttributeError):
-        renderapi.utils.renderdumps(pytest)
+        renderapi.utils.renderdumps(np.zeros(3))
