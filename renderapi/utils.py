01f6b965a9bcc4423da9df2ee881bc4351f607cd
Dan Kapner <danielk@.alleninstitute.org>
2018-06-19 16:12:29 -0700

    removing trailing L's in hex from encode/decode

--- renderapi/utils.py
+++ renderapi/utils.py
@@ -381,13 +381,13 @@ def encodeBase64(src):
     for ix in src:
         bits = bitstring.BitArray(float=ix, length=64).uint
         s += chr(bits >> 56)
-        s += chr(bits >> 48 & 0xffL)
-        s += chr(bits >> 40 & 0xffL)
-        s += chr(bits >> 32 & 0xffL)
-        s += chr(bits >> 24 & 0xffL)
-        s += chr(bits >> 16 & 0xffL)
-        s += chr(bits >> 8 & 0xffL)
-        s += chr(bits & 0xffL)
+        s += chr(bits >> 48 & 0xff)
+        s += chr(bits >> 40 & 0xff)
+        s += chr(bits >> 32 & 0xff)
+        s += chr(bits >> 24 & 0xff)
+        s += chr(bits >> 16 & 0xff)
+        s += chr(bits >> 8 & 0xff)
+        s += chr(bits & 0xff)
     zs = zlib.compress(s)
     encoded = base64.b64encode(zs)
     return encoded
@@ -415,21 +415,21 @@ def decodeBase64(src, n):
     j = 0
     for i in range(n):
         bits = 0L
-        bits += (ord(bvalues[j]) & 0xffL) << 56
+        bits += (ord(bvalues[j]) & 0xff) << 56
         j += 1
-        bits += (ord(bvalues[j]) & 0xffL) << 48
+        bits += (ord(bvalues[j]) & 0xff) << 48
         j += 1
-        bits += (ord(bvalues[j]) & 0xffL) << 40
+        bits += (ord(bvalues[j]) & 0xff) << 40
         j += 1
-        bits += (ord(bvalues[j]) & 0xffL) << 32
+        bits += (ord(bvalues[j]) & 0xff) << 32
         j += 1
-        bits += (ord(bvalues[j]) & 0xffL) << 24
+        bits += (ord(bvalues[j]) & 0xff) << 24
         j += 1
-        bits += (ord(bvalues[j]) & 0xffL) << 16
+        bits += (ord(bvalues[j]) & 0xff) << 16
         j += 1
-        bits += (ord(bvalues[j]) & 0xffL) << 8
+        bits += (ord(bvalues[j]) & 0xff) << 8
         j += 1
-        bits += ord(bvalues[j]) & 0xffL
+        bits += ord(bvalues[j]) & 0xff
         j += 1
         arr.append(bitstring.BitArray(uint=bits, length=64).float)
     return numpy.array(arr)
