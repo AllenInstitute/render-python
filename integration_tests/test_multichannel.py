5689a24ff4ca7018345c48a7d93d3986678f8064
Eric Perlman <perlman@gmail.com>
2019-05-28 09:59:01 -0700

    Basic test to test multiple channels with pointmatch client.

--- integration_tests/test_multichannel.py
+++ integration_tests/test_multichannel.py
@@ -32,9 +32,56 @@ def multichannel_test_stack(render):
     renderapi.stack.set_stack_state(stack, 'COMPLETE', render=render)
     return stack
 
+@pytest.fixture(scope='module')
+def test_pm_collection(render):
+    collection = 'test_multichan_collection'
+    renderapi.pointmatch.import_matches(
+        collection, test_matches, render=render)
+    return collection
+
 
 def test_section_image_channels(render, multichannel_test_stack):
     section_image = renderapi.image.get_section_image(multichannel_test_stack,
                                                       1.0, channel='DAPI',
                                                       render=render)
     print(section_image.shape)
+
+
+def test_multichannel_pointmatch_same_channel(render, multichannel_test_stack):
+    collection = 'test_multichannel_same_channel_collection'
+    sift_options = renderapi.client.SiftPointMatchOptions(renderScale=.25)
+    tile_pairs = [['100000001003000', '100000001004000']]
+    renderapi.client.pointMatchClient(multichannel_test_stack,
+                    collection,
+                    tile_pairs,
+                    filter=False,
+                    excludeAllTransforms=True,
+                    stackChannels='DAPI',
+                    sift_options=sift_options,
+                    render=render)
+    pms = renderapi.pointmatch.get_matches_involving_tile(
+        collection, collection, '100000001003000', render=render)
+    assert(len(pms) > 0)
+
+
+def test_multichannel_pointmatch_different_channel(render, multichannel_test_stack):
+    collection = 'test_multichannel_pointmatch_different_channel'
+    sift_options = renderapi.client.SiftPointMatchOptions(renderScale=.25)
+    tile_pairs = [['100000001003000', '100000001004000']]
+    renderapi.client.pointMatchClient(multichannel_test_stack,
+                    collection,
+                    tile_pairs,
+                    stack2=multichannel_test_stack,
+                    filter=False,
+                    excludeAllTransforms=True,
+                    stackChannels='DAPI',
+                    stack2Channels='TdTomato',
+                    sift_options=sift_options,
+                    render=render)
+    try:
+        pms = renderapi.pointmatch.get_matches_involving_tile(
+            collection, collection, '100000001003000', render=render)
+        assert(len(pms) == 0)
+    except renderapi.errors.RenderError:
+        # The point match collection should not exist.
+        return
