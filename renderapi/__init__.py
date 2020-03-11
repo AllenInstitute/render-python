d3cfa7df253240d17f858ee5b7f1c3bb078f9d99
Eric Trautman <trautmane@janelia.hhmi.org>
2020-03-11 08:26:06 -0400

    Bump version: 2.2.0 → 2.3.0

--- renderapi/__init__.py
+++ renderapi/__init__.py
@@ -13,7 +13,7 @@ from . import resolvedtiles
 from .render import connect
 from .render import Render
 
-__version__ = "2.2.0"
+__version__ = "2.3.0"
 __all__ = ['render', 'client', 'tilespec', 'errors',
            'stack', 'image', 'pointmatch', 'coordinate',
            'connect', 'transform', 'resolvedtiles', 'Render']
